2ded0cd6ab300deddce48fcf6db73547408875e3
Joy Pillai <54683754+joypillai@users.noreply.github.com>
2021-09-21 12:02:03 +0530

    Update main2.py

--- Angaad1/main2.py
+++ Angaad1/main2.py
@@ -10,6 +10,15 @@ import _thread
 import serial 
 import os
 
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+GPIO.setup(11,GPIO.OUT)
+GPIO.setup(13,GPIO.OUT)
+GPIO.setup(15,GPIO.OUT)
+GPIO.output(11,True)
+GPIO.output(13,True)
+
 item = ""
 top = 0
 bottom = 0
@@ -22,7 +31,11 @@ pos_servo0=0
 pos_servo1=0
 pos_servo2=0
 pos_servo3=0
+stoptimer=1
+stopfunction=0
+stop_arm=0
 val = 1
+run = 0
 time.sleep(5)
 #try:
    # arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=10)
@@ -49,7 +62,7 @@ def servo_run():
    
     for i in range(0,20,2):
         myKit.servo[2].angle=i
-        print("clockwise 1")
+        #print("clockwise 1")
         #print(i)
         time.sleep(0.05)
         pos_servo2=i
@@ -67,12 +80,44 @@ def servo_run():
         # print("clockwise 0")
         # print(pos_servo1)
         time.sleep(0.05)
-   
-    # for i in range(90,70,-1):
-    #     myKit.servo[2].angle=i
-    #     print("clockwise 2")
-    #     print(i)
-    #     time.sleep(0.05)
+
+
+def servo_initialize():
+    global pos_servo0
+    global pos_servo1
+    global pos_servo2
+    global pos_servo3
+    
+    myKit=ServoKit(channels=16)
+    if(pos_servo2<5):
+        for pos_servo2 in range(pos_servo2,5,1):
+            myKit.servo[2].angle=pos_servo2
+            time.sleep(0.05)
+
+    elif(pos_servo2>5):
+        for pos_servo2 in range(pos_servo2,5,-1):
+            myKit.servo[2].angle=pos_servo2
+            time.sleep(0.05)
+
+    if(pos_servo3<25):
+        for pos_servo3 in range(pos_servo3,25,1):
+            myKit.servo[3].angle=pos_servo3
+            time.sleep(0.05)
+
+    elif(pos_servo3>25):
+        for pos_servo3 in range(pos_servo3,25,-1):
+            myKit.servo[3].angle=pos_servo3
+            time.sleep(0.05)
+
+    if(pos_servo1<30):
+        for pos_servo1 in range(pos_servo1,30,1):
+            myKit.servo[1].angle=pos_servo1
+            time.sleep(0.05)
+
+    elif(pos_servo1>30):
+        for pos_servo1 in range(pos_servo1,30,-1):
+            myKit.servo[1].angle=pos_servo1
+            time.sleep(0.05)
 
 def lock_on():
     global item
@@ -84,6 +129,9 @@ def lock_on():
     global pos_servo1
     global pos_servo2
     global pos_servo3
+    global stopfunction
+    global stoptimer
+    global stop_arm 
     toplock = 0
     bottomlock = 0
     rightlock = 0
@@ -91,11 +139,12 @@ def lock_on():
     stop_arm = 0
     count = 0
     myKit=ServoKit(channels=16)
-    while (stop_arm == 0):
-         
-         if(stopBot==2):
+    while (stop_arm == 0): 
+        
+        if (stopfunction==1):
+            stopfunction=0
             break
-         
+        print("stop fn = ",stopfunction)
         print("lock on running")
         print(item,top,left,bottom,right)
 
@@ -140,6 +189,7 @@ def lock_on():
                 myKit.servo[2].angle=pos_servo2
             elif(item=="Redbox" and area>=340000):
                     stop_arm=1
+                    stoptimer=1
                
                 
         leftlock=left
@@ -208,72 +258,52 @@ def main1() :
     global stopBot
     while True:
         
+        while arduino.in_waiting:
         
-        print("thread1 running \n")
-        for i in range (1,2,1):
-            
-            data = arduino.readline()
-            #if data:
-            #print(data)
-            numbers = data.decode('utf-8')
-            arr = []
-            for word in numbers.split():
-                if word.isdigit():
-                    arr.append(int(word))
-            # for element in data:
-            #     numbers=numbers+element
-            # print("\n")
-            # numbers = numbers[0:4]
-            if (len(arr)==0):
-                continue
-            distance = arr[0]
-            
-            if (val == 1):
-                Front = distance
-                val = 2
-            elif (val == 2):
-                stopBot = distance
-                val = 1
+            print("thread1 running....................... \n")
+            for i in range (1,2,1):
                 
-            # elif (val == 3):
-            #     Back = distance
-            #     val = 4
-            # elif (val == 4):
-            #     Left = distance
-            #     val = 1
-            print(stopBot)
-            print("\n")
-            serial.Serial.reset_input_buffer
-            time.sleep(0.05)
+                data = arduino.readline()
+                #if data:
+                #print(data)
+                numbers = data.decode('utf-8')
+                arr = []
+                for word in numbers.split():
+                    if word.isdigit():
+                        arr.append(int(word))
+                
+
+                if (len(arr)==0):
+                    continue
+                distance = arr[0]
+                
+                if (val == 1):
+                    stopBot = distance
+                    val = 2
+                elif (val == 2):
+                    Front = distance
+                    val = 1
+                    
+                
+                print("stopBot value: ",stopBot)
+                print("\n")
+                #if (arduino.in_waiting > 4):
+                    #serial.Serial.reset_input_buffer()
+                
             
                 
-            # if (stopBot==1):
-            #     time.sleep(5)
-            #     arData='Y'
-            #     byteData=bytes(arData, 'utf-8')
-            #     arduino.write(byteData)
-            #     stopBot=0
-            #     arData='N'
-            # else:
-            #     arData='N'
-            #     byteData=bytes(arData, 'utf-8')
-            #     arduino.write(byteData)
 
 def sendY():
     global controlSend
 
     if (controlSend==1):
-        time.sleep(5)
-        arData='Y'
-        byteData=bytes(arData, 'utf-8')
-        arduino.write(byteData)
-        stopBot=0
-        arData='N'
-        controlSend = 0
+        GPIO.output(11,True)
+        #GPIO.output(13,False)
+        #GPIO.output(15,False)
     else:
-        arData='N'
-        byteData=bytes(arData, 'utf-8')
-        arduino.write(byteData)
+        GPIO.output(11,False)
+        #GPIO.output(13,False)
+        #GPIO.output(15,False)
 
 def object_detection():
 
@@ -316,7 +346,7 @@ def object_detection():
             bottom=int(detect.Bottom)
             right=int(detect.Right)
             item=net.GetClassDesc(ID)
-            print(item,top,left,bottom,right)
+            print("object detection: ",item,top)
             #time.sleep(1)
             cv2.rectangle(img,(left,top),(right,bottom),(255,0,0),2)
             cv2.putText(img,item,(left,top+20),font,.75,(0,0,255),2)
@@ -325,7 +355,7 @@ def object_detection():
         timeStamp=time.time()
         fps=1/dt
         fpsFiltered=0.9*fpsFiltered+.1*fps
-        print(str(round(fps,1))+' fps')
+        #print(str(round(fps,1))+' fps')
 
         cv2.putText(img,str(round(fpsFiltered,1))+' fps',(0,30),font,1,(0,0,255),2)
         cv2.imshow('detCam',img)
@@ -367,48 +397,109 @@ def servo_place():
         # print(pos_servo1)
         time.sleep(0.05)
 
-    for pos_servo2 in range(pos_servo2,20,-2):   
+    for pos_servo2 in range(pos_servo2,5,-2):   
         myKit.servo[2].angle=pos_servo2
         # print("clockwise 2")
         # print(pos_servo2)
         time.sleep(0.05)
 
-    for pos_servo3 in range(pos_servo3,23,-1):
+    for pos_servo3 in range(pos_servo3,25,-1):
         myKit.servo[3].angle=pos_servo3
         # print("clockwise 0")
         # print(pos_servo1)
         time.sleep(0.05)
 
+def timeexceed():
+    global stoptimer
+    global stopfunction
+    while True:
+        timecount=0
+        while(stoptimer==0):
+            print("*********timer running*******")
+            time.sleep(1)
+            timecount=timecount+1
+            if(timecount>=60):
+                stoptimer=1
+                stopfunction=1
+def check_top():
+    global top
+    global run
+    localtop=top
+    time.sleep(0.5)
+    if(localtop==top):
+        run=0
+    else:
+        run=1
+
+
 if __name__=="__main__":
-    # global stopBot
-    # global controlSend
-    # global pick
     time.sleep(1)
-    picked = 0
     thread1 = threading.Thread(target=main1)
     thread2 = threading.Thread(target=object_detection)
-    #thread3 = threading.Thread(target=main2)
+    thread3 = threading.Thread(target=timeexceed)
     #thread2 = threading.Thread(target=servo_run)
     thread1.start()
     thread2.start()
+    thread3.start()
     servo_run()
+    servo_initialize()
     while True:
-        controlSend=0
-        sendY()
-        if (stopBot == 1):
+        print("controlSend= ",controlSend)
+        print("pick= ",pick)
+        print("StopBot = ",stopBot)
+        if (stopBot == 1 and pick == 0):
             #thread2.start()
+            print("pick function~~~#####")
+            controlSend=0
+            sendY()
+            stoptimer=0
+            stopfunction=0
             lock_on()
-            if(stopBot!=2):
-               pick_up()
-               pick=1
-               #thread2.join()
-               controlSend = 1
-               sendY()
-               stopBot=0
-               time.sleep(0.05)
-        elif (stopBot == 2):
+            stoptimer=1
+            if(stop_arm==1):
+                stop_arm=0
+                pick_up()
+                pick=1
+                stopBot=0
+            servo_initialize()
+            top=0
+            controlSend=1
+            sendY()
+            stoptimer=1
+            stopfunction=0
+            time.sleep(0.05)
+
+        elif (stopBot == 2 and pick == 1):
+            print("place function$$$$$$$$***$$$$$$")
+            controlSend = 0
+            sendY()
             servo_place()
+            servo_initialize()
             controlSend = 1
             sendY()
             stopBot=0
+            top=0
+            pick=0
             time.sleep(0.05)
+            
+
+        elif (top>200 and pick==0):
+            print("SEnddddd Function0000000")
+            GPIO.output(13,False)
+            run=0
+            check_top()
+            if(run==1):
+                GPIO.output(13,True)
+                controlSend = 0
+                sendY()
+                run=0
+            else:
+                top=0
+            GPIO.output(13,True)
+            time.sleep(0.05)
+            
+        else:
+            print("else function")
+            controlSend = 1
+            sendY()
+          
